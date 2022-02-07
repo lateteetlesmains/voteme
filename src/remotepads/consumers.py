cef2ea9018fba179b9fe0809b30f12a12659c65f
Damien Lebouvier <damien@lebouvier.fr>
2022-02-07 13:41:08 +0100

    Modif urls pour avoir une page d'accueil

--- src/remotepads/consumers.py
+++ src/remotepads/consumers.py
@@ -2,59 +2,44 @@ from email import message
 import json
 from tkinter import N
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
-
-# class PadConsumer(WebsocketConsumer):
-#     def connect(self):
-#         self.accept()
-
-#     def disconnect(self, close_code):
-#         pass
-
-#     def receive(self, text_data):
-#         text_data_json = json.loads(text_data)
-#         message = text_data_json['message']
-
-#         self.send(text_data=json.dumps({
-#             'message': message
-#         }))
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class PadConsumer(WebsocketConsumer):
-    def connect(self):
+class PadConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         print(self.room_name)
         self.romm_group_name = 'chat_%s' % self.room_name
         # Joint le groupe
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.romm_group_name,
             self.channel_name
         )
         
-        self.accept()
-        
+        await self.accept()
+
        
 
-    def disconnect(self, code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(
             self.romm_group_name,
             self.channel_name
         )
 
-    def receive(self, text_data=None):
+    async def receive(self, text_data=None):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        message = text_data_json['id'] + ":" + text_data_json['message']
 
-    
-        async_to_sync(self.channel_layer.group_send)(
+        print(message)
+        await self.channel_layer.group_send(
             self.romm_group_name,
             {
                 'type': 'chat_message',
                 'message': message
             }
         )
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event['message']
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message
         }))
--- src/remotepads/views.py
+++ src/remotepads/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html', {'room_name': 'pads'})
+    return render(request, 'index.html', {})
 
 def pads(request):
-    return render(request, 'padclient.html')
+    return render(request, 'padclients.html',{'room_name': 'pads'})
