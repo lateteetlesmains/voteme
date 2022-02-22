7f1a6a8fc5a985a46953de299f889be05a3df94d
Damien Lebouvier <damien@lebouvier.fr>
2022-02-22 14:02:33 +0100

    Modif ui couleurs LTELM + ajout sons

--- src/remotepads/static/js/padclients.js
+++ src/remotepads/static/js/padclients.js
@@ -55,18 +55,42 @@ webSocket.onmessage = function (e) {
         if (waiting_players) {
             if (players.find(elt => elt.id == data.id)) return; //On s'assure que c'est pas le même joueur qui s'inscrit
             $('#waiting_players').addClass('hidden');
+            $('#players_container').removeClass('hidden');
             var incomingPlayer = new Player(data.id, undefined);
+
             players.push(incomingPlayer);
             incomingPlayer.number = players.indexOf(incomingPlayer) + 1;
 
-            $('#pad_container').append(`
+            $('#players_container').append(`
             
-                <div class='pads' id="player_${incomingPlayer.number}">
-                    <div class="center" id="${incomingPlayer.number}">Joueur ${incomingPlayer.number}</div>
-                                        
+            <div class='box-Buzzer' id='box_Buzzer${incomingPlayer.number}'>
+               
+                    <div class="player_name_score_container">
+                        <div class='player_name'>
+                            <h2>Joueur ${incomingPlayer.number}</h2>
+                        </div>
+                        <div class='score_container'>
+                            <div>
+                                <label class='score' >SCORE </label>
+                                <label class='score' >: 0 Points</label>  
+                            </div>
+                                           
+                            <input class='btn btn-primary' id='btn_buzzer_${incomingPlayer.number}plus' type='submit' name='btn-Buzzer_${incomingPlayer.number}_+' value='-1'>
+                            <input class='btn btn-primary' id='btn_buzzer_${incomingPlayer.number}reset' type='submit' name='btn-Buzzer_reset' value='Remise à zéro'>
+                            <input class='btn btn-primary' id='btn_buzzer_${incomingPlayer.number}minus' type='submit' name='btn-Buzzer_${incomingPlayer.number}_-' value='+1'>
+                        </div>
+                    </div>
+                                      
+                <div class='audio_container'>
+                    <audio src="/static/audio/smb_jump-super.wav" type="audio/mpeg">
+                            Your browser does not support the audio element.
+                    </audio>
                 </div>
+                
+            </div>
+
             `);
-            // <div id="${incomingPlayer.number}_answer">${incomingPlayer.answer == undefined ? "" : incomingPlayer.answer}</div>
+
         }
 
         else if (gameStarted) {
@@ -77,7 +101,7 @@ webSocket.onmessage = function (e) {
             if (!player.has_answered) {
                 //On evite de pouvoir changer sa réponse 
                 if (currentGameMode == GameModes.QCM) {
-                    player.answer = new ExpectedAnswer(data.message); 
+                    player.answer = new ExpectedAnswer(data.message);
                     // player.answer = !player.has_answered ? new ExpectedAnswer(data.message) : player.answer;
 
                     $('#' + player.number + '_answer').text(player.answer);
@@ -92,8 +116,8 @@ webSocket.onmessage = function (e) {
                     quick_players.push(player);
                     player.rank = quick_players.indexOf(player);
                     // if (quick_players.length == players.length) {
-                        //tout le monde a joué
-                    if(!$('#player_' + quick_players[0].number).hasClass('good_answer'))
+                    //tout le monde a joué
+                    if (!$('#player_' + quick_players[0].number).hasClass('good_answer'))
                         $('#player_' + quick_players[0].number).addClass('good_answer');
                     // }
                     log(quick_players);
@@ -119,25 +143,25 @@ webSocket.onclose = function (e) {
 
 
 $(() => {
-    $('#question_type_form').on('change',function (e) {
+    $('#question_type_form').on('change', function (e) {
         currentGameMode = e.target.id == "qcm" ? GameModes.QCM : GameModes.Quick;
         if (currentGameMode == GameModes.Quick)
             $('#answer_form').addClass('hidden');
         else
             $('#answer_form').removeClass('hidden');
     });
-    $('#answer_form').on('change',function (e) {
+    $('#answer_form').on('change', function (e) {
         expected_answer = new ExpectedAnswer(e.target.id);
         log(expected_answer);
     })
 
-    $('#start_btn').on('click',function (e) {
-        message = { 'id': 'admin', 'message': "start" }
+    $('#start_btn').on('click', function (e) {
+        message = { 'id': 'admin', 'message': "start" };
+        log($(e.target).val());
         if ($(e.target).val() == "Démarrer") {
             //Attente des joueurs
             $(e.target).val('Lancer la partie');
-            $('#init_container').addClass("hidden");
-            $('.form').removeClass('hidden');
+            $('#start_text').addClass("hidden");
             $('#waiting_players').removeClass('hidden');
             message.message = "start"
             waiting_players = true;
@@ -152,20 +176,20 @@ $(() => {
             message.message = "game"
             waiting_players = false;
             gameStarted = true;
-            $('.game').removeClass("hidden");
+            // $('.game').removeClass("hidden");
 
         }
         else {
             //reset
-            $('#init_container').removeClass("hidden");
-            $('.form').addClass('hidden');
+            $('#start_text').removeClass("hidden");
+            // $('.form').addClass('hidden');
             $('#waiting_players').addClass('hidden');
             $(e.target).val("Démarrer");
             message.message = "reset"
             waiting_players = false;
             gameStarted = false;
             players = [];
-            $('#pad_container').text('');
+            $('#players_container').text('');
             $('.game').addClass("hidden");
 
         }
