cc81f41e8cbaa32616136faf489bf4e97b4f6951
Damien Lebouvier <damien@lebouvier.fr>
2022-08-17 16:46:36 +0200

    Allumage led lors du démarrage serveur

--- src/remotepads/consumers.py
+++ src/remotepads/consumers.py
@@ -30,6 +30,7 @@ class ScreensThread(Thread):
         self.lastNbPlayers = -1
         self.daemon = True
         d.clear()
+        d.draw(3,'0',Colors.Green.color())
 
     def run(self):
         while True:
@@ -42,6 +43,7 @@ class ScreensThread(Thread):
                     self.lastNbPlayers = 0
             elif waiting:
                 if(self.lastNbPlayers != len(pads)):
+                    d.clear()
                     d.draw(1, 'p', Colors.Blue.color())
                     d.draw(5, len(pads), colors[len(pads) - 1].color())
                     self.lastNbPlayers = len(pads)
@@ -120,6 +122,9 @@ class PadConsumer(AsyncWebsocketConsumer):
                 self.started = True
                 waiting = True
                 d.clear()
+                # d.draw(2, '.', Colors.Purple.color())
+                # d.draw(3, '.', Colors.Purple.color())
+                # d.draw(4, '.', Colors.Purple.color())
             elif incoming.message == 'game':
                 self.ingame = True
                 waiting = False
@@ -141,6 +146,7 @@ class PadConsumer(AsyncWebsocketConsumer):
                 waiting = False
                 pads = []
                 d.clear()
+                d.draw(3,'0',Colors.Green.color())
                 thread.lastNbPlayers = -1
             elif incoming.message == "OK":
                 # on renvoie la bonne couleur au pad à qui on dit ok
